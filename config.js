9073854214a2d00f997ea15cb558f71f2623cd16
amalikhsan <iikhsamal@gmail.com>
2022-12-26 05:17:01 +0700

    nice

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     API_KEY_RM_BG: "c18s2XMGXQjFgx5x7hS4iR7r",
-    API_KEY_OPEN_AI: "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",
+    API_KEY_OPEN_AI: "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",
 };
